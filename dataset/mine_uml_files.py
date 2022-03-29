643346e6e1539fa7c4c839c7cc06356059d78505
hjalmar <hjalmar.thunberg@gmail.com>
2022-03-29 19:12:55 +0200

    Fix better structure and visualization when mining

--- dataset/mine_uml_files.py
+++ dataset/mine_uml_files.py
@@ -1,49 +1,68 @@
 '''
 Given a CSV file named projects_with_uml with a list of projects with UML files in them,
-mine and store these UML files using wget of extensions .xmi and .uml.
+mine and store these UML files using wget of desired extensions.
+
+Prerequisites:
+- wget
+- tqdm
 '''
 
 import csv
 import os
+from tqdm import tqdm
 
-# Given a CSV file named projects_with_uml
+already = []
+
+# Count lines in projects_with_uml.csv
+with open('data/projects_with_uml.csv', 'r') as csvfile:
+    lines = len(csvfile.readlines())
+
+# Given a CSV file named projects_with_uml.csv
 with open('data/projects_with_uml.csv', 'r') as csvfile:
 
+    # Create dataset directory and save path
     os.chdir('data')
-    curr_dir = os.getcwd()
+    if not os.path.exists('dataset'):
+        os.makedirs('dataset')
+    os.chdir('dataset')
+    dataset_dir = os.getcwd()
 
-    # Create extention folders and error log
-    if not os.path.exists('uml'):
-        os.makedirs('uml')
-    if not os.path.exists('xmi'):
-        os.makedirs('xmi')
+    # Create new/ clear existing error log
     with open('error_log.txt', 'w') as f:
         f.write('')
 
-    # Mine .xmi and .uml UML files in projects_with_uml
-    for uml_files in csv.reader(csvfile):
+    # Mine desired UML files from projects_with_uml.csv, displayed with a progress bar
+    for uml_files in tqdm(csv.reader(csvfile), total=lines, position=0, leave=True):
 
-        # Skip UML image files
-        # if 'https://git.eclipse.org/' in uml_files[0] or
-        if uml_files[1].endswith('.png') or uml_files[1].endswith('.gif') or uml_files[1].endswith('.jpg') or uml_files[1].endswith('.jpeg'):
-            continue
+        # Add project name to progressbar
+        project_name = str(uml_files[0].split("/")[1])
 
-        # Change to correct extention directory
-        if uml_files[1].endswith('.uml'):
-            os.chdir('uml')
-        elif uml_files[1].endswith('.xmi'):
-            os.chdir('xmi')
+        if uml_files[1] not in already:
+            # Skip UML image files
+            # if uml_files[1].endswith('.png') or uml_files[1].endswith('.gif') or uml_files[1].endswith('.jpg') or uml_files[1].endswith('.jpeg'):
+            #    continue
 
-        # wget --timeout=seconds --tries=number -q --show-progress [input file]
-        wget = ' '.join(
-            ['wget', '--timeout=3', '--tries=2', uml_files[1]])
-        status = os.system(wget)
+            # Create current project directory
+            if not os.path.exists(project_name):
+                os.makedirs(project_name)
+            os.chdir(project_name)
 
-        # Go back to data folder
-        os.chdir(curr_dir)
+            # wget --timeout=seconds --tries=number -q [input file]
+            wget = ' '.join(
+                ['wget', '--timeout=3', '--tries=2', '-q', '--show-progress', ((uml_files[1].replace('(', '_')).replace(')', '_')).replace(' ', '_')])
+            status = os.system(wget)
 
-        # Write in error_log.txt if failed to mine current UML file
-        if status != 0:
-            with open('error_log.txt', 'a') as f:
-                f.write(str(status) + ': ' + uml_files[1])
-                f.write('\n')
+            # Go back to data folder
+            os.chdir(dataset_dir)
+
+            # Remove project directory if empty
+            if len(os.listdir(project_name)) == 0:
+                os.rmdir(project_name)
+            else:
+                already.append(uml_files[1])
+
+            # Write in error_log.txt if failed to mine current UML file
+            if status != 0:
+                with open('error_log.txt', 'a') as f:
+                    f.write(str(status) + ': ' + uml_files[1])
+                    f.write('\n')
