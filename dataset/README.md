aff53fe84133eb35271ea64daf0680703a4e0459
hjalmar <hjalmar.thunberg@gmail.com>
2022-04-04 14:55:31 +0200

    Add new version of dataset

--- dataset/README.md
+++ dataset/README.md
@@ -1,6 +1,13 @@
 # Dataset
+
 Mine UML files from GitHub given a CSV file named projects_with_uml with a list of projects with UML files.
 When runned, will mine and store UML files using wget of type .xmi and .uml
 
 ## Setup
+
+Install wget
+Install tqdm `pip install tqdm`
+
+## Run
+
 Run script `python mine_uml_files.py`
--- dataset/mine_uml_files.py
+++ dataset/mine_uml_files.py
@@ -36,33 +36,34 @@ with open('data/projects_with_uml.csv', 'r') as csvfile:
 
         # Add project name to progressbar
         project_name = str(uml_files[0].split("/")[1])
+        if not project_name:
+            project_name = 'other'
 
-        if uml_files[1] not in already:
-            # Skip UML image files
-            # if uml_files[1].endswith('.png') or uml_files[1].endswith('.gif') or uml_files[1].endswith('.jpg') or uml_files[1].endswith('.jpeg'):
-            #    continue
+        # Skip already mined projects and projects from eclipse
+        if uml_files[1] not in already and 'https://git.eclipse.org/' not in uml_files[0]:
 
-            # Create current project directory
+            # Create project directory
             if not os.path.exists(project_name):
                 os.makedirs(project_name)
             os.chdir(project_name)
 
             # wget --timeout=seconds --tries=number -q [input file]
             wget = ' '.join(
-                ['wget', '--timeout=3', '--tries=2', '-q', '--show-progress', ((uml_files[1].replace('(', '_')).replace(')', '_')).replace(' ', '_')])
+                ['wget', '--timeout=3', '--tries=2', '-q', '--show-progress', uml_files[1].replace(' ', '_')])
             status = os.system(wget)
 
             # Go back to data folder
             os.chdir(dataset_dir)
 
-            # Remove project directory if empty
-            if len(os.listdir(project_name)) == 0:
-                os.rmdir(project_name)
-            else:
-                already.append(uml_files[1])
+            already.append(uml_files[1])
 
             # Write in error_log.txt if failed to mine current UML file
             if status != 0:
                 with open('error_log.txt', 'a') as f:
                     f.write(str(status) + ': ' + uml_files[1])
                     f.write('\n')
+
+    # Remove empty folders
+    for entry in os.scandir(dataset_dir):
+        if len(os.listdir(entry)) == 0:
+            os.rmdir(entry)
